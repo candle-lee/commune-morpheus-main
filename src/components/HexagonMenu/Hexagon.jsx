697eb7031cfd84d443eed1c1df50fdba1cc10924
candle-lee <bri.candle.li@gmail.com>
2024-04-01 06:28:18 +0900

    feat: create home section

--- src/components/HexagonMenu/Hexagon.jsx
+++ src/components/HexagonMenu/Hexagon.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Hexagon = ({ runLoader, title, icon, url }) => {
+const Hexagon = ({ title, icon, url }) => {
   return (
     <>
       <div className="hexagon-item">
@@ -14,22 +14,34 @@ const Hexagon = ({ runLoader, title, icon, url }) => {
           <div></div>
           <div></div>
         </div>
-        <a href={url} className="hex-content" onClick={() => runLoader()}>
+        <a href={url} className="hex-content" onClick={() => {}}>
           <span className="hex-content-inner">
             <span className="icon">
               <i className={icon}></i>
             </span>
             <span className="title">{title}</span>
           </span>
-          <svg
-            viewBox="0 0 173.20508075688772 200"
-            height="200"
-            width="174"
+          {/* <svg
+            viewBox="0 0 120.24355652982148 140"
+            height="140"
+            width="120.24355652982148"
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z"
+              d="M60.121778264910744 0L120.24355652982148 35L120.24355652982148 105L60.121778264910744 140L0 105L0 35Z"
+              fill="#1e2530"
+            ></path>
+          </svg> */}
+          <svg
+            viewBox="0 0 138.56406460551017 160"
+            height="160"
+            width="138.56406460551017"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M69.28203230275509 0L138.56406460551017 40L138.56406460551017 120L69.28203230275509 160L0 120L0 40Z"
               fill="#1e2530"
             ></path>
           </svg>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,18 @@ const Home = () => {
     return (
         <section id="home" className="page-home">
             <div className="overlay">
-                <HexagonMenu/>
+                <div className="container">
+                    <div className="home-header">
+                        <HexagonMenu />
+                        <div class="page-title home text-center">
+                            <h2>CommuneAI 
+                                <span class="primary"> Morpheus</span> 
+                                <span class="title-bg">COMMUNEAI</span>
+                            </h2>
+                            <p>Anyone can be a contributer, anyone can be a enjoyer</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
